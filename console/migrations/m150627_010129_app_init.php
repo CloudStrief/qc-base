15c19346d28de9b86109f89440a8bfbbc2a61c19
legendjw <legendjww@gmail.com>
2015-06-29 10:02:01 +0800

    增加权限、菜单数据库迁移，修改初始化数据库迁移

--- console/migrations/m150627_010129_app_init.php
+++ console/migrations/m150627_010129_app_init.php
@@ -1,15 +1,25 @@
 <?php
+/**
+ * @link http://www.u-bo.com/
+ * @copyright 南京友博网络科技有限公司 
+ * @license http://www.u-bo.com/license/
+ */
 
 use yii\db\Schema;
 use yii\db\Migration;
 
-class m150227_152353_init_app extends Migration
+/**
+ * 应用初始化迁移
+ *
+ * @author legendjw <legendjww@gmail.com>
+ * @since 0.1
+ */
+class m150627_010129_app_init extends Migration
 {
     public function up()
     {
-        $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=MyISAM';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=MyISAM';
         }
 
         $this->createTable('{{%admin}}', [
@@ -21,13 +31,15 @@ class m150227_152353_init_app extends Migration
             'password_hash' => Schema::TYPE_STRING . ' NOT NULL DEFAULT \'\' COMMENT \'加盐的密码\'',
             'password_reset_token' => Schema::TYPE_STRING . ' NOT NULL DEFAULT \'\' COMMENT \'重置密码token\'',
 
-            'status' => Schema::TYPE_BOOLEAN . ' NOT NULL DEFAULT 0 COMMENT \'状态,启用为1禁用为0\'',
+            'status' => Schema::TYPE_BOOLEAN . ' NOT NULL DEFAULT 0 COMMENT \'状态:启用为1,禁用为0\'',
+            'sort' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 0 COMMENT \'排序\'',
             'login_times' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0 COMMENT \'登录次数\'',
             'login_error_times' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0 COMMENT \'登录失败次数\'',
             'last_login_ip' => Schema::TYPE_INTEGER . '(10) NOT NULL DEFAULT 0 COMMENT \'最后登录ip地址\'',
             'last_login_time' => Schema::TYPE_INTEGER . '(10) NOT NULL DEFAULT 0 COMMENT \'最后登录时间\'',
             'last_modify_password_time' => Schema::TYPE_INTEGER . '(10) NOT NULL DEFAULT 0 COMMENT \'最后修改密码时间\'',
             'create_time' => Schema::TYPE_INTEGER . '(10) NOT NULL DEFAULT 0 COMMENT \'创建时间\'',
+            'create_user_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0 COMMENT \'创建人\'',
         ], $tableOptions . ' COMMENT=\'后台管理员表\'');
 
         //新增一个超级管理员
---
+++ console/migrations/m150627_015837_menu_init.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @link http://www.u-bo.com/
+ * @copyright 南京友博网络科技有限公司 
+ * @license http://www.u-bo.com/license/
+ */
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+/**
+ * 菜单初始化迁移
+ *
+ * @author legendjw <legendjww@gmail.com>
+ * @since 0.1
+ */
+class m150627_015837_menu_init extends Migration
+{
+    public function up()
+    {
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=MyISAM';
+        }
+
+        $this->createTable('{{%menu}}', [
+            'menu_id' => Schema::TYPE_PK . ' COMMENT \'菜单ID\'',
+            'name' => Schema::TYPE_STRING . ' NOT NULL DEFAULT \'\' COMMENT \'菜单名称\'',
+            'parent_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0 COMMENT \'父级ID\'',
+            'parent_ids' => Schema::TYPE_STRING . ' NOT NULL DEFAULT \'\' COMMENT \'所有父级ID\'',
+            'child_ids' => Schema::TYPE_STRING . ' NOT NULL DEFAULT \'\' COMMENT \'所有子级ID\'',
+            'app' => Schema::TYPE_STRING . '(20) NOT NULL DEFAULT \'\' COMMENT \'APP名称\'',
+            'module' => Schema::TYPE_STRING . '(30) NOT NULL DEFAULT \'\' COMMENT \'模块名称\'',
+            'controller' => Schema::TYPE_STRING . '(30) NOT NULL DEFAULT \'\' COMMENT \'控制器名称\'',
+            'action' => Schema::TYPE_STRING . '(30) NOT NULL DEFAULT \'\' COMMENT \'动作名称\'',
+            'params' => Schema::TYPE_STRING . '(30) NOT NULL DEFAULT \'\' COMMENT \'附加参数\'',
+            'auth_item' => Schema::TYPE_STRING . '(64) NOT NULL DEFAULT \'\' COMMENT \'认证项\'',
+            'auth_rules' => Schema::TYPE_STRING . '(100) NOT NULL DEFAULT \'\' COMMENT \'认证规则\'',
+            'status' => Schema::TYPE_BOOLEAN . ' NOT NULL DEFAULT 0 COMMENT \'状态:显示为1,隐藏为0\'',
+            'sort' => Schema::TYPE_SMALLINT . ' NOT NULL DEFAULT 0 COMMENT \'排序\'',
+            'type' => Schema::TYPE_SMALLINT . '(1) NOT NULL DEFAULT 0 COMMENT \'菜单类型:只做菜单为0,菜单+权限节点为1\'',
+            'remark' => Schema::TYPE_STRING . ' NOT NULL DEFAULT \'\' COMMENT \'备注\'',
+            'create_time' => Schema::TYPE_INTEGER . '(10) NOT NULL DEFAULT 0 COMMENT \'创建时间\'',
+            'create_user_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 0 COMMENT \'创建人\'',
+
+        ], $tableOptions . ' COMMENT=\'菜单表\'');
+
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%menu}}');
+    }
+}
