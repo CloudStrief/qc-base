f78fce6b7db791f4530047b134d40395f21de09e
CloudStrief <mzhuanxiang@sina.com>
2015-09-24 14:28:45 +0800

    增加日期选择控件

---
+++ common/assets/My97DatePickerAsset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link http://www.u-bo.com
+ * @copyright 南京友博网络科技有限公司 
+ * @license http://www.u-bo.com/license/
+ */
+
+namespace common\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * my97日期插件资源包
+ *
+ * @author strief <strief@u-bo.com>
+ * @since 0.1
+ */
+class My97DatePickerAsset extends AssetBundle
+{
+    public $basePath = '@webroot/bundle';
+    public $baseUrl = '@web/bundle';
+    public $css = [
+    ];
+    public $js = [
+        'js/my97DatePicker/WdatePicker.js',
+    ];
+}
